41c5e08769fe43dae0d328dc2c73a7fac15c737d
Muneeb Aslam <Muneebaslam470@gmail.com>
2026-06-04 18:30:55 +0500

    User logout

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 urlpatterns = [
     path('login/', views.LoginPage, name="login"),
+    path('logout/', views.LogoutUser, name="logout"),
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/', views.CreateRoom, name="create-room"),
