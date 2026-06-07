c5644ea79857c68658ab3382fe3fd04193891890
Muneeb Aslam <Muneebaslam470@gmail.com>
2026-06-07 23:31:59 +0500

    User account page edit

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from .models import Room , Topic, message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # Create your views here.
 
@@ -168,3 +168,18 @@ def deleteMessage(request, pk):
         Message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':Message})
+
+
+@login_required(login_url= 'login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id )
+    
+    context = {'form':form }
+    return render(request, 'base/update-user.html' , context )
